5bdf2db05f851b8009d11fb889a60617eda65c9b
Alexei Chmelev <alexei_chmelev@gmx.de>
2025-12-25 18:34:23 +0100

    GUI for the SSL extension implemented, the SSL Extension ist complete

--- src/main/java/com/tcpviewer/ui/controller/MainController.java
+++ src/main/java/com/tcpviewer/ui/controller/MainController.java
@@ -161,7 +161,7 @@ public class MainController {
                         dialogController.getLocalIp(),
                         dialogController.getLocalPort(),
                         dialogController.getTargetHost(),
-                        dialogController.getTargetPort(), false, null
+                        dialogController.getTargetPort(), dialogController.isSSLEnabled(), dialogController.getSSLHost()
                 );
 
                 proxyService.startProxySession(session);
